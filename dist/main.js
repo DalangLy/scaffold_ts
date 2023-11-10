9d90ac9bb6ce820e2d3cc87e20fc6cfc00978dcf
dalangly <Dalangly@outlook.com>
2023-11-10 11:13:57 +0700

    setup express typescript

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,108 @@
 "use strict";
-console.log("Hello World!");
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const http = __importStar(require("http"));
+const morgan_1 = __importDefault(require("morgan"));
+const path = __importStar(require("path"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const node_sass_middleware_1 = __importDefault(require("node-sass-middleware"));
+const http_errors_1 = __importDefault(require("http-errors"));
+const debug_1 = __importDefault(require("debug"));
+const app = (0, express_1.default)();
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use((0, morgan_1.default)('dev'));
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: false }));
+app.use((0, cookie_parser_1.default)());
+app.use((0, node_sass_middleware_1.default)({
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public'),
+    indentedSyntax: true,
+    sourceMap: true
+}));
+app.use(express_1.default.static(path.join(__dirname, 'public')));
+app.use('/', (req, res) => {
+    res.render('index', { title: 'Express' });
+});
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next((0, http_errors_1.default)(404));
+});
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+const PORT = 3000;
+app.set('port', PORT);
+/**
+ * Create HTTP server.
+ */
+let server = http.createServer(app);
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(PORT);
+server.on('error', onError);
+server.on('listening', onListening);
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+    var bind = typeof PORT === 'number'
+        ? 'Pipe ' + PORT
+        : 'Port ' + PORT;
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+function onListening() {
+    var addr = server.address();
+    var bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+    (0, debug_1.default)('Listening on ' + bind);
+}
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,98 @@
 import express, {Application} from "express";
 import * as http from "http";
+import morgan from "morgan";
+import * as path from "path";
+import cookieParser from "cookie-parser";
+import nodeSassMiddleware from "node-sass-middleware";
+import createHttpError from "http-errors";
+import debug from "debug";
+import helmet from "helmet";
+import compression from "compression";
 
 const app: Application = express();
-const server: http.Server = http.createServer(app);
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(helmet());
+app.use(compression());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(nodeSassMiddleware({
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public'),
+    indentedSyntax: true, // true = .sass and false = .scss
+    sourceMap: true
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', (req, res) => {
+    res.render('index', { title: 'Express' });
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createHttpError(404));
+});
+
+// error handler
+app.use(function(err: any, req: any, res: any, next: any) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+const PORT: number = 3000;
+app.set('port', PORT);
+
+/**
+ * Create HTTP server.
+ */
+
+let server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(PORT);
+server.on('error', onError);
+server.on('listening', onListening);
+
+function onError(error: any) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    var bind = typeof PORT === 'number'
+        ? 'Pipe ' + PORT
+        : 'Port ' + PORT;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+function onListening() {
+    var addr = server.address();
+    var bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr!.port;
+    debug('Listening on ' + bind);
+}
